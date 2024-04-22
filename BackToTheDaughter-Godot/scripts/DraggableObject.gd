2f95dfedb0d64cf16e147a2ddeb2ebc1b5e717c8
tomi <herceg133@gmail.com>
2024-04-22 22:11:02 +0200

    Created First Look of Draggable Objects

---
+++ BackToTheDaughter-Godot/scripts/DraggableObject.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var selected = false
+@export var smoothValue = 25
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		selected = true
+
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), smoothValue * delta)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			selected = false
